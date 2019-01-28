94d36065dd52dac390a051b4b38bd60ab4efedb7
Rob Tucker <rtucker@mozilla.com>
2019-01-28 08:28:14 -0500

    Moved return_matches into new function and file

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ extern crate serde_derive;
 mod system;
 mod minv_config;
 mod inventory_api;
-use clap::{App, Arg, SubCommand};
+mod return_matches;
+
 
 
 fn main() {
@@ -20,73 +21,7 @@ fn main() {
     let config = minv_config::get_config(home_path);
     // let _yaml = load_yaml!("cli.yml");
     // let matches = App::from_yaml(_yaml).get_matches();
-        let matches = App::new("minvtool")
-        .about("CLI interface to Mozilla Inventory")
-        .version("0.0.1")
-        .author("Rob Tucker <rtucker@mozilla.com>")
-        .subcommand(SubCommand::with_name("system")
-            .about("Intefaces with System Objects")
-            .subcommand(SubCommand::with_name("get")
-                .about("Get System")
-                .arg(Arg::with_name("hostname")
-                    .required(true)
-                    .help("Hostname of System to be retrieved")
-                )
-            )
-            .subcommand(SubCommand::with_name("delete")
-                .about("Delete System Objects")
-                .arg(Arg::with_name("hostname")
-                    .required(true)
-                    .help("Hostname of System to be created")
-                )
-            )
-            .subcommand(SubCommand::with_name("update")
-                .about("Updates System Objects")
-                .arg(Arg::with_name("hostname")
-                    .required(true)
-                    .help("Hostname of System to be created")
-                )
-                .arg(Arg::with_name("serial")
-                    .required(false)
-                    .long("serial")
-                    .help("System serial number")
-                    .takes_value(true)
-                )
-            )
-            .subcommand(SubCommand::with_name("create")
-                .about("Creates System Objects")
-                .arg(Arg::with_name("hostname")
-                    .required(true)
-                    .help("Hostname of System to be created")
-                )
-                .arg(Arg::with_name("server-model-id")
-                    .required(false)
-                    .long("server-model-id")
-                    .help("ID of server-model")
-                    .takes_value(true)
-                )
-                .arg(Arg::with_name("server-model-name")
-                    .required(false)
-                    .long("server-model-name")
-                    .help("Name of server-model")
-                    .takes_value(true)
-                )
-                .arg(Arg::with_name("serial")
-                    .required(false)
-                    .long("serial")
-                    .help("System serial number")
-                    .takes_value(true)
-                )
-                .arg(Arg::with_name("asset-tag")
-                    .required(false)
-                    .long("asset-tag")
-                    .help("System Asset Tag")
-                    .takes_value(true)
-                )
-            )
-            
-        )
-        .get_matches();
+    let matches = return_matches::return_matches();
 
     match matches.subcommand_matches("system") {
         Some(value) => { system::execute(value, config.clone()) },
